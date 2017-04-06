b55dd9d19df82927996566e9c4584791bf0619e7
Zhenyi He <snowymohehe@gmail.com>
2017-04-06 00:29:32 -0400

    use quaternion instead of eular angle

--- Assets/PhoneController/Scripts/RemoteControlledObjectExample.cs
+++ Assets/PhoneController/Scripts/RemoteControlledObjectExample.cs
@@ -2,27 +2,18 @@
 
 public class RemoteControlledObjectExample : MonoBehaviour {
 
-    //public Quaternion offset = Quaternion.identity;
-    public Vector3 offset = new Vector3(0, 0, 0);
+    public Quaternion offset = Quaternion.identity;
+    
 	void Update () {
-        Vector3 eulerReceived = AGRemoteController.RemoteControllerReceiver.Instance.Orientation.eulerAngles;
-        eulerReceived -= offset;
-        this.transform.localRotation = Quaternion.Euler(eulerReceived);
-        //this.transform.localRotation = AGRemoteController.RemoteControllerReceiver.Instance.Orientation * offset;
-        if (AGRemoteController.RemoteControllerReceiver.Instance.GetKeyDown('R'))
+	    if (AGRemoteController.RemoteControllerReceiver.Instance.GetKeyDown('R'))
         {
-            offset = AGRemoteController.RemoteControllerReceiver.Instance.Orientation.eulerAngles;
-            offset.x -= 90; // forward recenter
-            //offset.SetFromToRotation(AGRemoteController.RemoteControllerReceiver.Instance.Orientation * Vector3.forward, Vector3.forward);
-            //Debug.LogWarning(offset);
-
-
+			offset = AGRemoteController.RemoteControllerReceiver.Instance.Orientation * Quaternion.Euler(-90,0,0);
         }		
-
-		if (AGRemoteController.RemoteControllerReceiver.Instance.GetKeyDown('A')) {
+		else if (AGRemoteController.RemoteControllerReceiver.Instance.GetKeyDown('A')) {
 			this.transform.localScale *= 1.1f;
 		} else if (AGRemoteController.RemoteControllerReceiver.Instance.GetKeyDown('B')) {
 			this.transform.localScale *= 0.9f;
 		}
+		transform.localRotation = AGRemoteController.RemoteControllerReceiver.Instance.Orientation * Quaternion.Inverse(offset);
 	}
 }
